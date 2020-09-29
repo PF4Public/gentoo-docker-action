2d4f61389be1df7be46a6654b081a61bad97521a
PF4Public <PF4Public@users.noreply.github.com>
2020-09-30 01:55:20 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
+echo "$1"
+
+echo "$*"
+
+echo "USE=\"${@:3}\" ebuild $oldest $2"
+
 cd $1
 oldest=$(ls -At --ignore="*9999*" --ignore=Manifest --ignore=metadata.xml --ignore=files | tail -n 1)
 
