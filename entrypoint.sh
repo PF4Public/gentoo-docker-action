df8a3cbe75fcb8136acde45628e7b5795550f222
PF4Public <PF4Public@users.noreply.github.com>
2020-09-30 02:19:20 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 emerge -qv gn
 
 cd "$1"
-oldest=$(ls -At --ignore="*9999*" --ignore=Manifest --ignore=metadata.xml --ignore=files | tail -n 1)
+newest=$(ls -Art --ignore="*9999*" --ignore=Manifest --ignore=metadata.xml --ignore=files | tail -n 1)
 
-USE="${@:3}" ebuild $oldest $2
+USE="${@:3}" ebuild $newest $2
