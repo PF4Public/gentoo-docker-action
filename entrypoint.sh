969748970a315eaba71086ce72077523ff76e739
PF4Public <PF4Public@users.noreply.github.com>
2020-10-03 21:55:24 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-echo -e "sort:\n"
+EXIT_CODE=0
+
+echo "sort:"
 (echo "$1" | grep -Po '"\K[^"]*.ebuild'; echo "$2" |grep -Po '"\K[^"]*.ebuild') | sort -du
 
-IFS=$'\n' combined=($((echo "$1" | grep -Po '"\K[^"]*.ebuild'; echo "$2" |grep -Po '"\K[^"]*.ebuild') | sort -du))
+list=$((echo "$1" | grep -Po '"\K[^"]*.ebuild'; echo "$2" |grep -Po '"\K[^"]*.ebuild') | sort -du)
+echo -e "list:\n$list\n"
+
+IFS=$'\n' combined=($list)
 
 echo -e "combined:\n$combined\n"
 echo -e "combined0:\n${combined[0]}\n"
@@ -12,4 +17,11 @@ for i in "${combined[@]}"
 do
 echo -e "i:\n$i\n"
    USE="${@:4}" ebuild $i $3
+   RETVAL=$?
+   if [ $RETVAL -ne 0 ]
+   then
+      EXIT_CODE=$RETVAL
+   fi
 done
+
+exit $EXIT_CODE
