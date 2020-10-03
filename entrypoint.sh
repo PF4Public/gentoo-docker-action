fd84b4419f1d82531bb8ffa99c5cab922c623c1c
PF4Public <PF4Public@users.noreply.github.com>
2020-10-03 04:09:32 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -l
 
+echo "files_modified.json:"
+cat ${HOME}/files_modified.json
+echo "files_added.json:"
+cat ${HOME}/files_added.json
+
 cd "$1"
 ls -la
 newest=$(ls -At *.ebuild | head -n 1)
