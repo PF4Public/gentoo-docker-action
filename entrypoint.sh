314733d6ad581ecc430b06b3b9a4d84ae6f1d1e4
PF4Public <PF4Public@users.noreply.github.com>
2020-10-03 23:13:29 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,25 +5,11 @@ EXIT_CODE=0
 USE_FLAGS="${@:4}"
 ACTION="$3"
 
-
-echo "sort:"
-(echo "$1" | grep -Po '"\K[^"]*.ebuild'; echo "$2" |grep -Po '"\K[^"]*.ebuild') | sort -du
-
 list=$((echo "$1" | grep -Po '"\K[^"]*.ebuild'; echo "$2" |grep -Po '"\K[^"]*.ebuild') | sort -du | tr '\n' ' ')
-echo -e "list:\n$list\n"
 
-combined=($list)
-
-echo -e "combined:\n$combined\n"
-echo -e "combined0:\n${combined[0]}\n"
-
-#for i in "${combined[@]}"
-#for i in "$list"
-#do
 set -- junk $list
 shift
 for i; do
-echo -e "i:\n$i\n"
    USE="$USE_FLAGS" ebuild $i $ACTION
    RETVAL=$?
    if [ $RETVAL -ne 0 ]
