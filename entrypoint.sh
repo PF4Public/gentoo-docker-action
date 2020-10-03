c9a1cfd6537c3db9f767e8ab7e0dcd4a9bb807fc
PF4Public <PF4Public@users.noreply.github.com>
2020-10-03 23:14:11 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 EXIT_CODE=0
 
@@ -7,6 +7,7 @@ ACTION="$3"
 
 list=$((echo "$1" | grep -Po '"\K[^"]*.ebuild'; echo "$2" |grep -Po '"\K[^"]*.ebuild') | sort -du | tr '\n' ' ')
 
+# No array support
 set -- junk $list
 shift
 for i; do
