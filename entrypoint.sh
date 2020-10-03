e5fd3bc3a6ee33c8b1804f12f52994f71e423a74
PF4Public <PF4Public@users.noreply.github.com>
2020-10-03 21:27:29 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,11 @@ IFS=$'\n'
 combined=($((echo "$1" | grep -Po '"\K[^"]*.ebuild'; echo "$2" |grep -Po '"\K[^"]*.ebuild') | sort -du))
 IFS="$OLDIFS"
 
-echo "$combined"
+echo -e "combined:\n$combined\n"
+echo -e "combined0:\n${combined[0]}\n"
 
 for i in "${combined[@]}"
 do
-echo "$i"
+echo -e "i:\n$i\n"
    USE="${@:4}" ebuild $i $3
 done
