50b6cd1a0c8b9ebde2d2199aac105a5a9a68c866
PF4Public <PF4Public@users.noreply.github.com>
2021-05-27 23:40:34 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,43 @@
 
 EXIT_CODE=0
 
-USE_FLAGS="${@:4}"
-ACTION="$3"
+TOOL="$3"
 
-list=$((echo "$1" | grep -Po '"\K[^"]*.ebuild'; echo "$2" |grep -Po '"\K[^"]*.ebuild') | sort -du | tr '\n' ' ')
+if [ "$TOOL" == "ebuild" ]; then
+    list=$((echo "$1" | grep -Po '"\K[^"]*.ebuild'; echo "$2" |grep -Po '"\K[^"]*.ebuild') | sort -du | tr '\n' ' ')
+    ACTION="$4"
+    USE_FLAGS="${@:5}"
+elif [ "$TOOL" == "repoman" ]; then
+    list=$((echo "$1" | grep -Po '"\K[^"]*(.ebuild|Manifest|.xml)'; echo "$2" |grep -Po '"\K[^"]*(.ebuild|Manifest|.xml)') | sort -du | tr '\n' ' ')
+    list=$(dirname "$list" | sort -du | uniq | tr '\n' ' ')
+    ACTION="$4"
+    PARAMS="${@:5}"
+elif [ "$TOOL" == "emerge" ]; then
+    echo "TODO!"
+    exit 1
+fi
 
 # No array support
 set -- junk $list
 shift
 for i; do
-   USE="$USE_FLAGS" NODIE=1 I_KNOW_WHAT_I_AM_DOING=yes ebuild $i $ACTION
-   RETVAL=$?
-   if [ $RETVAL -ne 0 ]
-   then
-      EXIT_CODE=$RETVAL
-   fi
+    if [ "$TOOL" == "ebuild" ]; then
+        USE="$USE_FLAGS" NODIE=1 I_KNOW_WHAT_I_AM_DOING=yes ebuild $i $ACTION
+        RETVAL=$?
+    elif [ "$TOOL" == "repoman" ]; then
+        push $i
+            repoman $ACTION $PARAMS
+            RETVAL=$?
+        pop
+    elif [ "$TOOL" == "emerge" ]; then
+        echo "TODO!"
+        exit 1
+    fi
+
+    if [ $RETVAL -ne 0 ]
+    then
+        EXIT_CODE=$RETVAL
+    fi
 done
 
 exit $EXIT_CODE
